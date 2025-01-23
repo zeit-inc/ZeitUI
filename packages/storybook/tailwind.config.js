6fbb90f9693086d5fc8847af92c6fe32d237fdfd
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-23 08:08:49 -0500

    Feat: Actualizar dependencias de Storybook y configurar Tailwind para usar la librería Heroui

--- packages/storybook/tailwind.config.js
+++ packages/storybook/tailwind.config.js
@@ -1,16 +1,21 @@
+import {heroui} from '@heroui/react';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    // Archivos de la librería de diseño
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
+
     // Archivos de Storybook
-    "./.storybook/**/*.{mdx,jsx,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './.storybook/**/*.{mdx,jsx,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
 
     // Componentes y sus stories
-    "../components/*/src/**/*.{js,jsx,ts,tsx}",
-    "../components/*/stories/**/*.{js,jsx,ts,tsx}",
+    '../components/*/src/**/*.{js,jsx,ts,tsx}',
+    '../components/*/stories/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [heroui()],
 };
