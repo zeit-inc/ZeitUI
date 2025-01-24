c1fadcdeb9092748cfbf26360c059a1d2b5551cf
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-24 10:16:13 -0500

    Feat: Añadir nuevas formas al icono de descarga y ajustar el formato del código

--- packages/icons/src/Download.tsx
+++ packages/icons/src/Download.tsx
@@ -1,4 +1,4 @@
-import { IconSvgProps } from "./types";
+import {IconSvgProps} from './types';
 
 export const Download = (props: IconSvgProps) => (
   <svg
@@ -13,6 +13,8 @@ export const Download = (props: IconSvgProps) => (
     strokeLinejoin="round"
     {...props}
   >
-    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
+    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+    <polyline points="7 10 12 15 17 10" />
+    <line x1="12" x2="12" y1="15" y2="3" />
   </svg>
 );
