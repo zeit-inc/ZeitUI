72bdc234607c4872117e055438244ace62c5c2ab
luislopez-developer <luislopezhuari27@gmail.com>
2025-01-28 10:10:36 -0500

    feat(icons): agregar nuevos íconos de Maximize, Minimize y Printer

---
+++ packages/icons/src/Maximize.tsx
@@ -0,0 +1,21 @@
+import { IconSvgProps } from "./types";
+
+export const Maximize = (props: IconSvgProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <polyline points="15 3 21 3 21 9"/>
+    <polyline points="9 21 3 21 3 15"/>
+    <line x1="21" x2="14" y1="3" y2="10"/>
+    <line x1="3" x2="10" y1="21" y2="14"/>
+  </svg>
+);
--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -2,8 +2,11 @@ export type {IconSvgProps} from './types';
 
 export * from './CircleX';
 export * from './Download';
+export * from './Maximize';
+export * from './Minimize';
 export * from './Minus';
 export * from './Plus';
+export * from './Printer';
 export * from './RotateCcw';
 export * from './RotateCw';
 export * from './Search';
